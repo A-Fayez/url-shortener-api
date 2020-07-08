0c399b1a11390457bc11670aad335c4d6955b2ba
a-fayez <ahmedf.higgy@gmail.com>
2020-07-08 17:37:33 +0200

    post and get use cases

--- api.py
+++ api.py
@@ -4,17 +4,18 @@ from flask import (
     url_for,
     redirect,
     request,
+    jsonify,
 )
 from dotenv import load_dotenv
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from helpers import generate_slug
+from helpers import generate_slug, valid_schema, save_url
 
 app = Flask(__name__)
 
-# load configuration for .env file
+# load configuration from .env file
 APP_ROOT = os.path.dirname(__file__)
 dotenv_path = os.path.join(APP_ROOT, ".env")
 load_dotenv(dotenv_path)
@@ -41,9 +42,81 @@ def index():
 @app.route("/shortlinks", methods=["POST", "GET"])
 def api():
     if request.method == "POST":
-        return f"post request: {request.json}"
+
+        try:
+            slug = request.json["slug"] or ""
+            ios_primary = request.json["ios"]["primary"]
+            ios_fallback = request.json["ios"]["fallback"]
+            android_primary = request.json["android"]["primary"]
+            android_fallback = request.json["android"]["fallback"]
+            web = request.json["web"]
+
+            # if any of the urls is invalid
+            if not valid_schema(
+                ios_primary=ios_primary,
+                ios_fallback=ios_fallback,
+                android_primary=android_primary,
+                android_fallback=android_fallback,
+                web=web,
+            ):
+                return jsonify({"bad request": 400}), 400
+
+            # if slug was not specified, generate it
+            if not slug:
+                slug = generate_slug(6)
+
+            # saves the valid url in db
+            save_url(
+                slug=slug,
+                ios_primary=ios_primary,
+                ios_fallback=ios_fallback,
+                android_primary=android_primary,
+                android_fallback=android_fallback,
+                web=web,
+                db=db,
+            )
+
+            return jsonify("url created successfully"), 200
+
+        except KeyError as e:
+            # if the slug key wasn't provided, generate it
+            if str(e.args[0]) == "slug":
+                slug = generate_slug(6)
+                save_url(
+                    slug=slug,
+                    ios_primary=ios_primary,
+                    ios_fallback=ios_fallback,
+                    android_primary=android_primary,
+                    android_fallback=android_fallback,
+                    web=web,
+                    db=db,
+                )
+                return (
+                    jsonify(
+                        {
+                            "success": True,
+                            "message": "url and its slug created successfully",
+                        }
+                    ),
+                    200,
+                )
+            else:
+                return (
+                    jsonify({"success": False, "message": "bad request"}),
+                    400,
+                )
 
     if request.method == "GET":
-        return "get request"
 
-    return "Test", 200
+        urls = db.execute(
+            "SELECT slug, \
+                            ios_primary_url, \
+                            ios_fallback_url, \
+                            android_primary_url, \
+                            android_fallback_url, \
+                            web_url \
+                            FROM urls"
+        ).fetchall()
+        print(type(urls))
+        print(urls)
+        return jsonify({"urls": [dict(url) for url in urls]})
--- helpers.py
+++ helpers.py
@@ -1,5 +1,75 @@
 import uuid
+import re
 
 
 def generate_slug(length=6):
+    """Generates a random char sequence for the slug"""
+
     return str(uuid.uuid4())[:length]
+
+
+def valid_schema(ios_primary, ios_fallback, android_primary, android_fallback, web):
+    """A helper function that checks some required args are presend
+        and checks if the fields are valid urls"""
+
+    if (
+        not ios_primary
+        and not ios_fallback
+        and not android_primary
+        and not android_fallback
+        and not web
+    ):
+        return False
+
+    else:
+        # django url validator
+        url_regex = re.compile(
+            r"^(?:http|ftp)s?://"  # http:// or https://
+            r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|"  # domain...
+            r"localhost|"  # localhost...
+            r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"  # ...or ip
+            r"(?::\d+)?"  # optional port
+            r"(?:/?|[/?]\S+)$",
+            re.IGNORECASE,
+        )
+
+        # if there is one wrong url
+        urls = [ios_primary, ios_fallback, android_primary, android_fallback, web]
+        for url in urls:
+            if not re.match(url_regex, url):
+                return False
+
+    return True
+
+
+def save_url(
+    slug, ios_primary, ios_fallback, android_primary, android_fallback, web, db
+):
+    "Saves the url in db"
+
+    db.execute(
+        "INSERT INTO urls( \
+                slug, \
+                ios_primary_url, \
+                ios_fallback_url, \
+                android_primary_url, \
+                android_fallback_url, \
+                web_url \
+            ) \
+            VALUES(:slug, \
+                    :ios_primary_url, \
+                    :ios_fallback_url, \
+                    :android_primary_url, \
+                    :android_fallback_url, \
+                    :web_url)",
+        {
+            "slug": slug,
+            "ios_primary_url": ios_primary,
+            "ios_fallback_url": ios_fallback,
+            "android_primary_url": android_primary,
+            "android_fallback_url": android_fallback,
+            "web_url": web,
+        },
+    )
+
+    db.commit()
